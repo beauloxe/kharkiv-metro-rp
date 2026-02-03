135fee05841810b75a0362fb6fc999df2637fc89
beauloxe <beauloxe@proton.me>
2026-02-03 20:31:48 +0200

    Update __init__.py

--- src/kharkiv_metro_rp/data/__init__.py
+++ src/kharkiv_metro_rp/data/__init__.py
@@ -1,7 +1,7 @@
 """Data module for Kharkiv Metro Route Planner."""
 
 from .database import MetroDatabase
-from .initializer import init_database, init_stations
+from .initializer import init_database, init_schedules, init_stations
 from .scraper import MetroScraper
 
-__all__ = ["MetroDatabase", "init_database", "init_stations", "MetroScraper"]
+__all__ = ["MetroDatabase", "init_database", "init_schedules", "init_stations", "MetroScraper"]
